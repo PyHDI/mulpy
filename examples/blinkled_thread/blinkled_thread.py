492b777794bd5c045d224af323055fc86d294f74
shtaxxx <shta.ky1018@gmail.com>
2016-02-18 00:02:59 +0900

    blinkled_thread example is updated.

--- examples/blinkled_thread/blinkled_thread.py
+++ examples/blinkled_thread/blinkled_thread.py
@@ -7,6 +7,7 @@ import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from mulpy import *
+import veriloggen as vg
 
 def mkBlinkled():
     m = Module('blinkled')
@@ -23,8 +24,40 @@ def mkBlinkled():
 
     return m
 
+def mkTest():
+    m = vg.Module('test')
+    
+    # target instance
+    led = mkBlinkled()
+    
+    # copy paras and ports
+    params = m.copy_params(led)
+    ports = m.copy_sim_ports(led)
+    
+    clk = ports['CLK']
+    rst = ports['RST']
+    
+    uut = m.Instance(led, 'uut',
+                     params=m.connect_params(led),
+                     ports=m.connect_ports(led))
+    
+    vg.simulation.setup_waveform(m, uut, m.get_vars())
+    vg.simulation.setup_clock(m, clk, hperiod=5)
+    init = vg.simulation.setup_reset(m, rst, m.make_reset(), period=100)
+    
+    init.add(
+        vg.Delay(1000 * 100),
+        vg.Systask('finish'),
+    )
+
+    return m
+
 if __name__ == '__main__':
-    main = mkBlinkled()
-    #vmod = main.to_veriloggen_module()
-    verilog = main.to_verilog('tmp.v')
-    print(verilog)
+    test = mkTest()
+    verilog = test.to_verilog('tmp.v')
+    #print(verilog)
+    
+    # run simulator (Icarus Verilog)
+    sim = vg.simulation.Simulator(test)
+    rslt = sim.run()
+    print(rslt)
